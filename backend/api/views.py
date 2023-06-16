9aa1617b658c11a6b39d6d65ba06a6b6fe2150cc
Abiola65 <abiolafolarin11@gmail.com>
2023-06-16 01:14:53 +0100

    my django project

---
+++ backend/api/views.py
@@ -0,0 +1,16 @@
+from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+
+@api_view(['POST'])
+def api_home(request, *args, **kwargs):
+    """"
+    DRF API View
+    """
+    data = request.data
+    return Response (data)
---
+++ py_client/basic.py
@@ -0,0 +1,16 @@
+import requests
+
+# endpoint = 'https://httpbin.org/status/200/'
+# endpoint = 'https://httpbin.org/anything'
+endpoint = 'http://127.0.0.1:8000/api/'
+
+get_response = requests.post(endpoint, params={'product_id': 123})
+# print (get_response.headers)
+# print(get_response.text)
+# print(get_response.status_code)
+
+#HTTP Request -> Html
+#REST API HTTP Request -> JSON
+#JASON = Javascript Object Notation ~ python 
+print(get_response.json())
+# print(get_response.status_code)
