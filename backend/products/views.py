d73ac43ee5662e4bb39c11cf20f631457ea2232a
Abiola65 <abiolafolarin11@gmail.com>
2023-06-18 09:28:14 +0100

    my changes

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,9 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from django.shortcuts import get_object_or_404
 
 from  .models import Product
 from.serializers import ProductSerializer
 
-class ProductCreateAPIView(generics.CreateAPIView):
+class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
@@ -15,7 +19,7 @@ class ProductCreateAPIView(generics.CreateAPIView):
             content = title
         serializer.save(content=content)
 
-product_create_view = ProductCreateAPIView.as_view()
+product_list_create_view = ProductListCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
@@ -23,3 +27,95 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     # lookup_field='pk'
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field='pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+        
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field='pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+        
+
+product_destroy_view = ProductDestroyAPIView.as_view()
+
+
+# class ProductListAPIView(generics.ListAPIView):
+#     """
+#     Not gonna use this method
+#     """
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+
+# product_list_view = ProductListAPIView.as_view()
+
+
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+    ):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = 'this is a  single view doing cool stuff'
+        serializer.save(content=content)
+
+    # def post()
+product_mixin_view = ProductMixinView.as_view()
+
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == 'GET':
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == 'POST':
+         serializer = ProductSerializer(data=request.data)
+         if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+    return Response({'invalid': 'not good data'}, status=400)
